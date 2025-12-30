ea6b4094c802fb4af1cfd3ae26cccef95f69ff07
Philip Giorgio <baseballstatistics@gmail.com>
2025-12-30 14:38:42 -0800

    Framing PinchGlobal component ... added PinchTypes class, PinchLogger class, and PinchMsgDlgs class.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,54 @@
-﻿using System;
+﻿#region HEADER
+//#####################################################################################################################
+//##############################################  P r o g r a m . c s  ################################################
+//#####################################################################################################################
+//  FILENAME:  Program.cs
+//  NAMESPACE: Pinch
+//  CLASS(S):  Program
+//  COMPONENT: Pinch.exe
+//=====================================================================================================================
+//  DESCRIPTION: 
+//    This file contains the code for the main entry point for the application.
+//=====================================================================================================================
+//  AUTHOR:
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//                                                                                                                   !!
+//                              A        JJJJJJJJ  PPPPPPP         EEEEEEE  NN     NN   GGGGGG                       !!
+//                             AAA          JJ     PP    PP        EE       NNN    NN  GG    GG                      !!
+//                            AA AA         JJ     PP    PP        EE       NNNN   NN  GG                            !!
+//                           AA   AA        JJ     PPPPPP          EEEEEEE  NN NN  NN  GG   GGGG                     !!
+//                          AAAAAAAA   JJ   JJ     PP              EE       NN  NN NN  GG    GG                      !!
+//                         AA      AA  JJ   JJ     PP              EE       NN    NNN  GG    GG                      !!
+//                        AA        AA  JJJJJJ     PP              EEEEEEE  NN     NN   GGGGGG                       !!
+//                                                                                                                   !!
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//    (c)Copyright 2026 AJP Engineering
+//    All rights reserved.
+//=====================================================================================================================
+//  HISTORY:
+//    01/01/26 .. pg .. Version 4.0
+//#####################################################################################################################
+//#####################################################################################################################
+//#####################################################################################################################
+#endregion      // HEADER
+
+#region REFERENCES
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using PinchGlobal;
+#endregion  // REFERENCES
+
+#region namespace Pinch
 namespace Pinch
 {
+    #region internal static class Program
+    /// <summary>
+    /// Program Class - main entry point for the application
+    /// </summary>
     internal static class Program
     {
         /// <summary>
@@ -16,7 +59,13 @@ namespace Pinch
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormMain());
         }
     }
+    #endregion      // internal static class Program
 }
+#endregion      // namespace Pinch
+
+//=====================================================================================================================
+//---------------------------------------------  E N D   O F   F I L E  -----------------------------------------------
+//=====================================================================================================================
