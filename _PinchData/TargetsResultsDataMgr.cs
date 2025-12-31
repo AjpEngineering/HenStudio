4752cb216452ee2a4a0b666695df8774388ed686
Philip Giorgio <baseballstatistics@gmail.com>
2025-12-31 12:47:27 -0800

    Framed DataMgr classes

--- _PinchData/TargetsResultsDataMgr.cs
+++ _PinchData/TargetsResultsDataMgr.cs
@@ -1,12 +1,98 @@
-﻿using System;
+﻿#region HEADER
+//#####################################################################################################################
+//##########################################  TargetsResultsDataMgr . c s  ############################################
+//#####################################################################################################################
+//  FILENAME:  TargetsResultsDataMgr.cs
+//  NAMESPACE: PinchData
+//  CLASS(S):  TargetsResultsDataMgr
+//  COMPONENT: _PinchData.dll
+//=====================================================================================================================
+//  DESCRIPTION: 
+//    This file contains the code for the Targets Results Data Manager class.
+//=====================================================================================================================
+//  AUTHOR:
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//                                                                                                                   !!
+//                              A        JJJJJJJJ  PPPPPPP         EEEEEEE  NN     NN   GGGGGG                       !!
+//                             AAA          JJ     PP    PP        EE       NNN    NN  GG    GG                      !!
+//                            AA AA         JJ     PP    PP        EE       NNNN   NN  GG                            !!
+//                           AA   AA        JJ     PPPPPP          EEEEEEE  NN NN  NN  GG   GGGG                     !!
+//                          AAAAAAAA   JJ   JJ     PP              EE       NN  NN NN  GG    GG                      !!
+//                         AA      AA  JJ   JJ     PP              EE       NN    NNN  GG    GG                      !!
+//                        AA        AA  JJJJJJ     PP              EEEEEEE  NN     NN   GGGGGG                       !!
+//                                                                                                                   !!
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//    (c)Copyright 2026 AJP Engineering
+//    All rights reserved.
+//=====================================================================================================================
+//  HISTORY:
+//    01/01/26 .. pg .. Version 4.0
+//#####################################################################################################################
+//#####################################################################################################################
+//#####################################################################################################################
+#endregion      // HEADER
+
+#region REFERENCES
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+#endregion  // REFERENCES
 
+#region namespace PinchData
 namespace PinchData
 {
+    #region public class TargetsResultsDataMgr
+    /// <summary>
+    /// Targets Results Data Manager Class
+    /// </summary>
     public class TargetsResultsDataMgr
     {
+        #region CONSTANTS
+        const string NAMESPACE = "PinchData";
+        const string CLASS = "TargetsResultsDataMgr";
+        #endregion      // CONSTANTS
+
+        #region ENUMS
+
+        //#region enum PinchUnits
+        ///// <summary>
+        ///// ENUMERATION: Pinch Units
+        ///// </summary>
+        //public enum PinchUnits
+        //{
+        //    ENGLISH,
+        //    METRIC
+        //};
+        //#endregion      // enum PinchUnits
+
+        #endregion      // ENUMS
+
+        #region FIELDS
+
+        #endregion      // FIELDS
+
+        #region PROPERTIES
+
+        #endregion      // PROPERTIES
+
+        #region CTOR
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public TargetsResultsDataMgr()
+        {
+            //string strMethod = "CTOR";
+            //ExchLogger.LogInfo(NAMESPACE, CLASS, strMethod, "Creating Object");
+        }
+        #endregion      // CTOR
+
     }
+    #endregion      // public class TargetsResultsDataMgr
 }
+#endregion      // namespace PinchData
+
+//=====================================================================================================================
+//---------------------------------------------  E N D   O F   F I L E  -----------------------------------------------
+//=====================================================================================================================
