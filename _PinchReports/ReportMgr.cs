b578a6f4da377e64728c10d5271ad11278392677
Philip Giorgio <baseballstatistics@gmail.com>
2025-12-31 13:05:59 -0800

    Frame Report Manager class

--- _PinchReports/ReportMgr.cs
+++ _PinchReports/ReportMgr.cs
@@ -1,12 +1,97 @@
-﻿using System;
+﻿#region HEADER
+//#####################################################################################################################
+//############################################  R e p o r t M g r . c s  ##############################################
+//#####################################################################################################################
+//  FILENAME:  ReportMgr.cs
+//  NAMESPACE: PinchReports
+//  CLASS(S):  ReportMgr
+//  COMPONENT: _PinchReports.dll
+//=====================================================================================================================
+//  DESCRIPTION: 
+//    This file contains the code for the Reports Manager class.
+//=====================================================================================================================
+//  AUTHOR:
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//                                                                                                                   !!
+//                              A        JJJJJJJJ  PPPPPPP         EEEEEEE  NN     NN   GGGGGG                       !!
+//                             AAA          JJ     PP    PP        EE       NNN    NN  GG    GG                      !!
+//                            AA AA         JJ     PP    PP        EE       NNNN   NN  GG                            !!
+//                           AA   AA        JJ     PPPPPP          EEEEEEE  NN NN  NN  GG   GGGG                     !!
+//                          AAAAAAAA   JJ   JJ     PP              EE       NN  NN NN  GG    GG                      !!
+//                         AA      AA  JJ   JJ     PP              EE       NN    NNN  GG    GG                      !!
+//                        AA        AA  JJJJJJ     PP              EEEEEEE  NN     NN   GGGGGG                       !!
+//                                                                                                                   !!
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//    (c)Copyright 2026 AJP Engineering
+//    All rights reserved.
+//=====================================================================================================================
+//  HISTORY:
+//    01/01/26 .. pg .. Version 4.0
+//#####################################################################################################################
+//#####################################################################################################################
+//#####################################################################################################################
+#endregion      // HEADER
+
+#region REFERENCES
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+#endregion  // REFERENCES
 
+#region namespace PinchReports
 namespace PinchReports
 {
+    #region public class ReportMgr
+    /// <summary>
+    /// Reports Manager Class
+    /// </summary>
     public class ReportMgr
     {
+        #region CONSTANTS
+        const string NAMESPACE = "PinchReports";
+        const string CLASS = "ReportMgr";
+        #endregion      // CONSTANTS
+
+        #region ENUMS
+
+        //#region enum PinchUnits
+        ///// <summary>
+        ///// ENUMERATION: Pinch Units
+        ///// </summary>
+        //public enum PinchUnits
+        //{
+        //    ENGLISH,
+        //    METRIC
+        //};
+        //#endregion      // enum PinchUnits
+
+        #endregion      // ENUMS
+
+        #region FIELDS
+
+        #endregion      // FIELDS
+
+        #region PROPERTIES
+
+        #endregion      // PROPERTIES
+
+        #region CTOR
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public ReportMgr()
+        {
+            //string strMethod = "CTOR";
+            //ExchLogger.LogInfo(NAMESPACE, CLASS, strMethod, "Creating Object");
+        }
+        #endregion      // CTOR
     }
+    #endregion      // public class ReportMgr
 }
+#endregion      // namespace PinchReports
+
+//=====================================================================================================================
+//---------------------------------------------  E N D   O F   F I L E  -----------------------------------------------
+//=====================================================================================================================
