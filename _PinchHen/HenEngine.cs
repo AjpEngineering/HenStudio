400c8cd7f2f351286002ee4f9a8d3e5b41447f98
Philip Giorgio <baseballstatistics@gmail.com>
2025-12-31 13:00:48 -0800

    Frame Hen Engine class

--- _PinchHen/HenEngine.cs
+++ _PinchHen/HenEngine.cs
@@ -1,12 +1,97 @@
-﻿using System;
+﻿#region HEADER
+//#####################################################################################################################
+//############################################  H e n E n g i n e . c s  ##############################################
+//#####################################################################################################################
+//  FILENAME:  HenEngine.cs
+//  NAMESPACE: PinchHen
+//  CLASS(S):  HenEngine
+//  COMPONENT: _PinchHen.dll
+//=====================================================================================================================
+//  DESCRIPTION: 
+//    This file contains the code for the HEN Engine class.
+//=====================================================================================================================
+//  AUTHOR:
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//                                                                                                                   !!
+//                              A        JJJJJJJJ  PPPPPPP         EEEEEEE  NN     NN   GGGGGG                       !!
+//                             AAA          JJ     PP    PP        EE       NNN    NN  GG    GG                      !!
+//                            AA AA         JJ     PP    PP        EE       NNNN   NN  GG                            !!
+//                           AA   AA        JJ     PPPPPP          EEEEEEE  NN NN  NN  GG   GGGG                     !!
+//                          AAAAAAAA   JJ   JJ     PP              EE       NN  NN NN  GG    GG                      !!
+//                         AA      AA  JJ   JJ     PP              EE       NN    NNN  GG    GG                      !!
+//                        AA        AA  JJJJJJ     PP              EEEEEEE  NN     NN   GGGGGG                       !!
+//                                                                                                                   !!
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//    (c)Copyright 2026 AJP Engineering
+//    All rights reserved.
+//=====================================================================================================================
+//  HISTORY:
+//    01/01/26 .. pg .. Version 4.0
+//#####################################################################################################################
+//#####################################################################################################################
+//#####################################################################################################################
+#endregion      // HEADER
+
+#region REFERENCES
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+#endregion  // REFERENCES
 
+#region namespace PinchHen
 namespace PinchHen
 {
+    #region public class HenEngine
+    /// <summary>
+    /// HEN Engine Class
+    /// </summary>
     public class HenEngine
     {
+        #region CONSTANTS
+        const string NAMESPACE = "PinchHen";
+        const string CLASS = "HenEngine";
+        #endregion      // CONSTANTS
+
+        #region ENUMS
+
+        //#region enum PinchUnits
+        ///// <summary>
+        ///// ENUMERATION: Pinch Units
+        ///// </summary>
+        //public enum PinchUnits
+        //{
+        //    ENGLISH,
+        //    METRIC
+        //};
+        //#endregion      // enum PinchUnits
+
+        #endregion      // ENUMS
+
+        #region FIELDS
+
+        #endregion      // FIELDS
+
+        #region PROPERTIES
+
+        #endregion      // PROPERTIES
+
+        #region CTOR
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public HenEngine()
+        {
+            //string strMethod = "CTOR";
+            //ExchLogger.LogInfo(NAMESPACE, CLASS, strMethod, "Creating Object");
+        }
+        #endregion      // CTOR
     }
+    #endregion      // public class HenEngine
 }
+#endregion      // namespace PinchHen
+
+//=====================================================================================================================
+//---------------------------------------------  E N D   O F   F I L E  -----------------------------------------------
+//=====================================================================================================================
