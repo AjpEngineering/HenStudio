bb5eafce5b1c1d29b12d48f436c3c43a121be0be
Philip Giorgio <baseballstatistics@gmail.com>
2025-12-31 12:55:46 -0800

    Frame Figures Manager class

--- _PinchFigures/FiguresMgr.cs
+++ _PinchFigures/FiguresMgr.cs
@@ -1,12 +1,98 @@
-﻿using System;
+﻿#region HEADER
+//#####################################################################################################################
+//###########################################  F i g u r e s M g r . c s  #############################################
+//#####################################################################################################################
+//  FILENAME:  FiguresMgr.cs
+//  NAMESPACE: PinchFigures
+//  CLASS(S):  FiguresMgr
+//  COMPONENT: _PinchFigures.dll
+//=====================================================================================================================
+//  DESCRIPTION: 
+//    This file contains the code for the Figures Manager class.
+//=====================================================================================================================
+//  AUTHOR:
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//                                                                                                                   !!
+//                              A        JJJJJJJJ  PPPPPPP         EEEEEEE  NN     NN   GGGGGG                       !!
+//                             AAA          JJ     PP    PP        EE       NNN    NN  GG    GG                      !!
+//                            AA AA         JJ     PP    PP        EE       NNNN   NN  GG                            !!
+//                           AA   AA        JJ     PPPPPP          EEEEEEE  NN NN  NN  GG   GGGG                     !!
+//                          AAAAAAAA   JJ   JJ     PP              EE       NN  NN NN  GG    GG                      !!
+//                         AA      AA  JJ   JJ     PP              EE       NN    NNN  GG    GG                      !!
+//                        AA        AA  JJJJJJ     PP              EEEEEEE  NN     NN   GGGGGG                       !!
+//                                                                                                                   !!
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//    (c)Copyright 2026 AJP Engineering
+//    All rights reserved.
+//=====================================================================================================================
+//  HISTORY:
+//    01/01/26 .. pg .. Version 4.0
+//#####################################################################################################################
+//#####################################################################################################################
+//#####################################################################################################################
+#endregion      // HEADER
+
+#region REFERENCES
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+#endregion  // REFERENCES
 
+#region namespace PinchFigures
 namespace PinchFigures
 {
+    #region public class FiguresMgr
+    /// <summary>
+    /// Figures Manager Class
+    /// </summary>
     public class FiguresMgr
     {
+        #region CONSTANTS
+        const string NAMESPACE = "PinchFigures";
+        const string CLASS = "FiguresMgr";
+        #endregion      // CONSTANTS
+
+        #region ENUMS
+
+        //#region enum PinchUnits
+        ///// <summary>
+        ///// ENUMERATION: Pinch Units
+        ///// </summary>
+        //public enum PinchUnits
+        //{
+        //    ENGLISH,
+        //    METRIC
+        //};
+        //#endregion      // enum PinchUnits
+
+        #endregion      // ENUMS
+
+        #region FIELDS
+
+        #endregion      // FIELDS
+
+        #region PROPERTIES
+
+        #endregion      // PROPERTIES
+
+        #region CTOR
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public FiguresMgr()
+        {
+            //string strMethod = "CTOR";
+            //ExchLogger.LogInfo(NAMESPACE, CLASS, strMethod, "Creating Object");
+        }
+        #endregion      // CTOR
+
     }
+    #endregion      // public class FiguresMgr
 }
+#endregion      // namespace PinchFigures
+
+//=====================================================================================================================
+//---------------------------------------------  E N D   O F   F I L E  -----------------------------------------------
+//=====================================================================================================================
