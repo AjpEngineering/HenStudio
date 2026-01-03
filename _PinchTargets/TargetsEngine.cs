e1e7010d40f1d58aed3f6f3e009d85c9b3d0e704
Philip Giorgio <baseballstatistics@gmail.com>
2026-01-03 10:54:42 -0800

    Frame TargetsEngine Class

--- _PinchTargets/TargetsEngine.cs
+++ _PinchTargets/TargetsEngine.cs
@@ -1,14 +1,85 @@
-﻿using System;
+﻿#region HEADER
+//#####################################################################################################################
+//########################################  T a r g e t s E n g i n e . c s  ##########################################
+//#####################################################################################################################
+//  FILENAME:  TargetsEngine.cs
+//  NAMESPACE: PinchTargets
+//  CLASS(S):  TargetsEngine
+//  COMPONENT: _PinchTargets.dll
+//=====================================================================================================================
+//  DESCRIPTION: 
+//    This file contains the code for the Targets Engine class.
+//=====================================================================================================================
+//  AUTHOR:
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//                                                                                                                   !!
+//                              A        JJJJJJJJ  PPPPPPP         EEEEEEE  NN     NN   GGGGGG                       !!
+//                             AAA          JJ     PP    PP        EE       NNN    NN  GG    GG                      !!
+//                            AA AA         JJ     PP    PP        EE       NNNN   NN  GG                            !!
+//                           AA   AA        JJ     PPPPPP          EEEEEEE  NN NN  NN  GG   GGGG                     !!
+//                          AAAAAAAA   JJ   JJ     PP              EE       NN  NN NN  GG    GG                      !!
+//                         AA      AA  JJ   JJ     PP              EE       NN    NNN  GG    GG                      !!
+//                        AA        AA  JJJJJJ     PP              EEEEEEE  NN     NN   GGGGGG                       !!
+//                                                                                                                   !!
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//    (c)Copyright 2026 AJP Engineering
+//    All rights reserved.
+//=====================================================================================================================
+//  HISTORY:
+//    01/01/26 .. pg .. Version 4.0
+//#####################################################################################################################
+//#####################################################################################################################
+//#####################################################################################################################
+#endregion      // HEADER
+
+#region REFERENCES
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using PinchGlobal;
+#endregion  // REFERENCES
 
+#region namespace PinchTargets
 namespace PinchTargets
 {
+    #region class TargetsEngine
+    /// <summary>
+    /// Targets Engine Class
+    /// </summary>
     public class TargetsEngine
     {
+        #region CONSTANTS
+        const string NAMESPACE = "PinchTargets";
+        const string CLASS = "TargetsEngine";
+        #endregion      // CONSTANTS
+
+        #region ENUMS
+
+        #endregion      // ENUMS
+
+        #region FIELDS
+
+        #endregion      // FIELDS
+
+        #region PROPERTIES
+
+        #endregion      // PROPERTIES
+
+        #region CTOR
+        public TargetsEngine() 
+        {
+            //string strMethod = "CTOR";
+            //ExchLogger.LogInfo(NAMESPACE, CLASS, strMethod, "Creating Object");
+        }
+        #endregion  // class CTOR
     }
+    #endregion  // class TargetsEngine
 }
+#endregion      // namespace PinchTargets
+
+//=====================================================================================================================
+//---------------------------------------------  E N D   O F   F I L E  -----------------------------------------------
+//=====================================================================================================================
