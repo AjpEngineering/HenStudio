12969add2daff728e61bd350cca8e4330ec2d2e8
Philip Giorgio <baseballstatistics@gmail.com>
2026-01-27 12:48:04 -0800

    Update FormLicenseScoreCard

--- FormScoreCard.Designer.cs
+++ FormScoreCard.Designer.cs
@@ -28,14 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle25 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle26 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormScoreCard));
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle27 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle28 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dataGridViewScoreCard = new System.Windows.Forms.DataGridView();
             this.ColumnID = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColumnState = new System.Windows.Forms.DataGridViewImageColumn();
             this.ColumnProperty = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.buttonOK = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewScoreCard)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,6 +47,9 @@
             // 
             this.dataGridViewScoreCard.AllowUserToAddRows = false;
             this.dataGridViewScoreCard.AllowUserToDeleteRows = false;
+            this.dataGridViewScoreCard.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridViewScoreCard.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridViewScoreCard.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dataGridViewScoreCard.BackgroundColor = System.Drawing.Color.Honeydew;
@@ -52,27 +59,27 @@
             this.dataGridViewScoreCard.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ColumnID,
             this.ColumnState,
-            this.ColumnProperty});
-            this.dataGridViewScoreCard.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridViewScoreCard.Location = new System.Drawing.Point(0, 0);
-            this.dataGridViewScoreCard.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ColumnProperty,
+            this.ColumnValue});
+            this.dataGridViewScoreCard.Location = new System.Drawing.Point(-1, 40);
+            this.dataGridViewScoreCard.Margin = new System.Windows.Forms.Padding(0);
             this.dataGridViewScoreCard.Name = "dataGridViewScoreCard";
             this.dataGridViewScoreCard.ReadOnly = true;
             this.dataGridViewScoreCard.RowHeadersVisible = false;
             this.dataGridViewScoreCard.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridViewScoreCard.Size = new System.Drawing.Size(284, 781);
+            this.dataGridViewScoreCard.Size = new System.Drawing.Size(564, 756);
             this.dataGridViewScoreCard.TabIndex = 0;
             // 
             // ColumnID
             // 
             this.ColumnID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
-            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle7.BackColor = System.Drawing.Color.Honeydew;
-            dataGridViewCellStyle7.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle7.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.Color.Black;
-            this.ColumnID.DefaultCellStyle = dataGridViewCellStyle7;
+            dataGridViewCellStyle25.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle25.BackColor = System.Drawing.Color.Honeydew;
+            dataGridViewCellStyle25.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle25.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle25.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle25.SelectionForeColor = System.Drawing.Color.Black;
+            this.ColumnID.DefaultCellStyle = dataGridViewCellStyle25;
             this.ColumnID.HeaderText = "ID";
             this.ColumnID.Name = "ColumnID";
             this.ColumnID.ReadOnly = true;
@@ -82,58 +89,112 @@
             // ColumnState
             // 
             this.ColumnState.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
-            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle8.BackColor = System.Drawing.Color.Honeydew;
-            dataGridViewCellStyle8.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle8.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle8.NullValue = ((object)(resources.GetObject("dataGridViewCellStyle8.NullValue")));
-            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.Color.Black;
-            this.ColumnState.DefaultCellStyle = dataGridViewCellStyle8;
-            this.ColumnState.HeaderText = "STATE";
+            dataGridViewCellStyle26.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle26.BackColor = System.Drawing.Color.Honeydew;
+            dataGridViewCellStyle26.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle26.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle26.NullValue = ((object)(resources.GetObject("dataGridViewCellStyle26.NullValue")));
+            dataGridViewCellStyle26.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle26.SelectionForeColor = System.Drawing.Color.Black;
+            this.ColumnState.DefaultCellStyle = dataGridViewCellStyle26;
+            this.ColumnState.HeaderText = "STATUS";
             this.ColumnState.MinimumWidth = 60;
             this.ColumnState.Name = "ColumnState";
             this.ColumnState.ReadOnly = true;
             this.ColumnState.Resizable = System.Windows.Forms.DataGridViewTriState.True;
             this.ColumnState.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
             this.ColumnState.ToolTipText = "License Property Status";
-            this.ColumnState.Width = 68;
+            this.ColumnState.Width = 77;
             // 
             // ColumnProperty
             // 
-            this.ColumnProperty.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle9.BackColor = System.Drawing.Color.Honeydew;
-            dataGridViewCellStyle9.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle9.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle9.Padding = new System.Windows.Forms.Padding(3);
-            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.Color.Black;
-            this.ColumnProperty.DefaultCellStyle = dataGridViewCellStyle9;
+            this.ColumnProperty.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridViewCellStyle27.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle27.BackColor = System.Drawing.Color.Honeydew;
+            dataGridViewCellStyle27.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle27.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle27.Padding = new System.Windows.Forms.Padding(3);
+            dataGridViewCellStyle27.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle27.SelectionForeColor = System.Drawing.Color.Black;
+            this.ColumnProperty.DefaultCellStyle = dataGridViewCellStyle27;
             this.ColumnProperty.HeaderText = "PROPERTY";
+            this.ColumnProperty.MinimumWidth = 60;
             this.ColumnProperty.Name = "ColumnProperty";
             this.ColumnProperty.ReadOnly = true;
             this.ColumnProperty.ToolTipText = "License Property";
+            this.ColumnProperty.Width = 94;
+            // 
+            // ColumnValue
+            // 
+            this.ColumnValue.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle28.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle28.BackColor = System.Drawing.Color.Honeydew;
+            dataGridViewCellStyle28.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle28.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle28.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle28.SelectionForeColor = System.Drawing.Color.Black;
+            this.ColumnValue.DefaultCellStyle = dataGridViewCellStyle28;
+            this.ColumnValue.HeaderText = "VALUE";
+            this.ColumnValue.MinimumWidth = 60;
+            this.ColumnValue.Name = "ColumnValue";
+            this.ColumnValue.ReadOnly = true;
+            this.ColumnValue.ToolTipText = "License File Property Value";
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxTitle.BackColor = System.Drawing.Color.OrangeRed;
+            this.textBoxTitle.Font = new System.Drawing.Font("Segoe UI Variable Display", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxTitle.ForeColor = System.Drawing.Color.White;
+            this.textBoxTitle.Location = new System.Drawing.Point(0, 2);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(564, 33);
+            this.textBoxTitle.TabIndex = 1;
+            this.textBoxTitle.Text = "License ScoreCard";
+            this.textBoxTitle.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOK.BackColor = System.Drawing.Color.White;
+            this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOK.FlatAppearance.BorderSize = 2;
+            this.buttonOK.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.buttonOK.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.buttonOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonOK.Font = new System.Drawing.Font("Segoe UI Variable Display", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonOK.ForeColor = System.Drawing.Color.OrangeRed;
+            this.buttonOK.Location = new System.Drawing.Point(5, 755);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(552, 36);
+            this.buttonOK.TabIndex = 2;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = false;
             // 
             // FormScoreCard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(284, 781);
+            this.BackColor = System.Drawing.Color.Honeydew;
+            this.ClientSize = new System.Drawing.Size(564, 801);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.textBoxTitle);
             this.Controls.Add(this.dataGridViewScoreCard);
             this.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(300, 820);
+            this.MinimumSize = new System.Drawing.Size(580, 840);
             this.Name = "FormScoreCard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "License Scorecard";
             this.Load += new System.EventHandler(this.FormScoreCard_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewScoreCard)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -143,5 +204,8 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnID;
         private System.Windows.Forms.DataGridViewImageColumn ColumnState;
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnProperty;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnValue;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.Button buttonOK;
     }
 }
--- FormScoreCard.cs
+++ FormScoreCard.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Principal;    // WindowsIdentity
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,31 +20,33 @@ namespace Pinch
 
         private void FormScoreCard_Load(object sender, EventArgs e)
         {
-            //-----------------------------
-            //--- DataGridViewScoreCard ---
-            //-----------------------------
-            dataGridViewScoreCard.Rows.Add("01", Properties.Resources.Valid_32x32, "Author");
-            dataGridViewScoreCard.Rows.Add("02", Properties.Resources.Valid_32x32, "Supplier Name");
-            dataGridViewScoreCard.Rows.Add("03", Properties.Resources.Valid_32x32, "Supplier URL");
-            dataGridViewScoreCard.Rows.Add("04", Properties.Resources.Valid_32x32, "Customer Name");
-            dataGridViewScoreCard.Rows.Add("05", Properties.Resources.Valid_32x32, "Customer Email");
-            dataGridViewScoreCard.Rows.Add("06", Properties.Resources.Valid_32x32, "Product Name");
-            dataGridViewScoreCard.Rows.Add("07", Properties.Resources.Valid_32x32, "Product Version");
-            dataGridViewScoreCard.Rows.Add("08", Properties.Resources.Valid_32x32, "Product Serial Number");
-            dataGridViewScoreCard.Rows.Add("09", Properties.Resources.Valid_32x32, "Product Code");
-            dataGridViewScoreCard.Rows.Add("10", Properties.Resources.Valid_32x32, "License Type");
-            dataGridViewScoreCard.Rows.Add("11", Properties.Resources.Valid_32x32, "Corporation");
-            dataGridViewScoreCard.Rows.Add("12", Properties.Resources.Valid_32x32, "Division");
-            dataGridViewScoreCard.Rows.Add("13", Properties.Resources.Valid_32x32, "Group");
-            dataGridViewScoreCard.Rows.Add("14", Properties.Resources.Valid_32x32, "User Name");
-            dataGridViewScoreCard.Rows.Add("15", Properties.Resources.Valid_32x32, "Domain");
-            dataGridViewScoreCard.Rows.Add("16", Properties.Resources.Valid_32x32, "Full Name");
-            dataGridViewScoreCard.Rows.Add("17", Properties.Resources.Valid_32x32, "Device Name");
-            dataGridViewScoreCard.Rows.Add("18", Properties.Resources.Valid_32x32, "License Duration");
-            dataGridViewScoreCard.Rows.Add("19", Properties.Resources.Valid_32x32, "License Start Date");
-            dataGridViewScoreCard.Rows.Add("20", Properties.Resources.Valid_32x32, "License End Date");
-            dataGridViewScoreCard.Rows.Add("21", Properties.Resources.Valid_32x32, "License Key");
-            dataGridViewScoreCard.Rows.Add("22", Properties.Resources.Valid_32x32, "License Hash");
+            string strUsername = Environment.UserName;
+            string strFullname = WindowsIdentity.GetCurrent().Name;
+            string strDomain = strFullname.Split('\\')[0];
+            string strDeviceName = Environment.MachineName;
+            //--------------------------------------
+            //--- DataGridViewScoreCard Controls ---
+            //--------------------------------------
+            dataGridViewScoreCard.Rows.Add("01", Properties.Resources.Valid_32x32, "Author", "AuthorValue");
+            dataGridViewScoreCard.Rows.Add("02", Properties.Resources.Valid_32x32, "Supplier Name", "SupplierNameValue");
+            dataGridViewScoreCard.Rows.Add("03", Properties.Resources.Valid_32x32, "Supplier URL", "SupplierURLValue");
+            dataGridViewScoreCard.Rows.Add("04", Properties.Resources.Valid_32x32, "Customer Name", "CustomerNameValue");
+            dataGridViewScoreCard.Rows.Add("05", Properties.Resources.Valid_32x32, "Customer Email", "CustomerEmailValue");
+            dataGridViewScoreCard.Rows.Add("06", Properties.Resources.Valid_32x32, "Product Name","ProductNameValue");
+            dataGridViewScoreCard.Rows.Add("07", Properties.Resources.Valid_32x32, "Product Version", "ProductVersionValue");
+            dataGridViewScoreCard.Rows.Add("08", Properties.Resources.Valid_32x32, "Product Serial Number", "ProductSerialNumberValue");
+            dataGridViewScoreCard.Rows.Add("09", Properties.Resources.Valid_32x32, "Product Code", "{3D9721BA-003E-4711-B7AF-B579645F0AC9}");
+            dataGridViewScoreCard.Rows.Add("10", Properties.Resources.Valid_32x32, "License Type", "LicenseTypeValue");
+            dataGridViewScoreCard.Rows.Add("11", Properties.Resources.Valid_32x32, "Corporation", "CorporationValue");
+            dataGridViewScoreCard.Rows.Add("12", Properties.Resources.Valid_32x32, "Division", "DivisionValue");
+            dataGridViewScoreCard.Rows.Add("13", Properties.Resources.Valid_32x32, "Group", "GroupValue");
+            dataGridViewScoreCard.Rows.Add("14", Properties.Resources.Valid_32x32, "User Name", "UserNameValue");
+            dataGridViewScoreCard.Rows.Add("15", Properties.Resources.Valid_32x32, "Device Name", "DeviceNameValue");
+            dataGridViewScoreCard.Rows.Add("16", Properties.Resources.Valid_32x32, "License Duration", "LicenseDurationValue");
+            dataGridViewScoreCard.Rows.Add("17", Properties.Resources.Valid_32x32, "License Start Date", "License Start Datevalue");
+            dataGridViewScoreCard.Rows.Add("18", Properties.Resources.Valid_32x32, "License End Date", "LicenseEndDateValue");
+            dataGridViewScoreCard.Rows.Add("19", Properties.Resources.Valid_32x32, "License Key", "LicenseKeyValue");
+            dataGridViewScoreCard.Rows.Add("20", Properties.Resources.Valid_32x32, "License Hash", "LicenseHashValue");
 
         }
     }
