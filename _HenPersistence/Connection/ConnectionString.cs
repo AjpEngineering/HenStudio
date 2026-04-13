03983a7c3e31b971d83ef0a71cc4f59ed2bd577a
Philip Giorgio <baseballstatistics@gmail.com>
2026-04-13 10:07:14 -0700

    Added ConnectionString.cs

---
+++ _HenPersistence/Connection/ConnectionString.cs
@@ -0,0 +1,54 @@
+﻿#region HEADER
+//#####################################################################################################################
+//######################################  C o n n e c t i o n S t r i n g . c s  ######################################
+//#####################################################################################################################
+//  FILENAME:  ConnectionString.cs
+//  NAMESPACE: HenPersistence.Connection
+//  CLASS(S):  ConnectionStrings
+//  COMPONENT: _HenPersistence.dll
+//=====================================================================================================================
+//  DESCRIPTION: 
+//    This file contains the database connection string constants for the persistence layer.
+//=====================================================================================================================
+//  AUTHOR:
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//                                                                                                                   !!
+//                              A        JJJJJJJJ  PPPPPPP         EEEEEEE  NN     NN   GGGGGG                       !!
+//                             AAA          JJ     PP    PP        EE       NNN    NN  GG    GG                      !!
+//                            AA AA         JJ     PP    PP        EE       NNNN   NN  GG                            !!
+//                           AA   AA        JJ     PPPPPP          EEEEEEE  NN NN  NN  GG   GGGG                     !!
+//                          AAAAAAAA   JJ   JJ     PP              EE       NN  NN NN  GG    GG                      !!
+//                         AA      AA  JJ   JJ     PP              EE       NN    NNN  GG    GG                      !!
+//                        AA        AA  JJJJJJ     PP              EEEEEEE  NN     NN   GGGGGG                       !!
+//                                                                                                                   !!
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//    (c)Copyright 2026 AJP Engineering
+//    All rights reserved.
+//=====================================================================================================================
+//  HISTORY:
+//    01/01/26 .. AJP Engineering .. Version 1.0
+//#####################################################################################################################
+//#####################################################################################################################
+//#####################################################################################################################
+#endregion      // HEADER
+
+#region namespace HenPersistence.Connection
+namespace HenPersistence.Connection
+{
+    #region public static class ConnectionStrings
+    /// <summary>
+    /// Database connection string constants class.
+    /// </summary>
+    public static class ConnectionStrings
+    {
+        #region CONSTANTS
+        public const string HenStudio = "Server=localhost\\SQLEXPRESS2025;Database=HenStudio;Trusted_Connection=True;";
+        #endregion      // CONSTANTS
+    }
+    #endregion      // public static class ConnectionStrings
+}
+#endregion      // namespace HenPersistence.Connection
+
+//=====================================================================================================================
+//---------------------------------------------  E N D   O F   F I L E  -----------------------------------------------
+//=====================================================================================================================
