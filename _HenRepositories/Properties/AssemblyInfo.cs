7de7981e2099e683540559f493c4f4502130c386
Philip Giorgio <baseballstatistics@gmail.com>
2026-05-05 11:12:55 -0700

    Consolidated _HenRepositories and _HenPersistence project into single _HenModel project (assembly) component

--- _HenRepositories/Properties/AssemblyInfo.cs
+++ _HenRepositories/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("_HenRepositories")]
-[assembly: AssemblyDescription("Heat Exchanger Network (HEN) Repositories Component")]
+[assembly: AssemblyTitle("_HenModel")]
+[assembly: AssemblyDescription("Heat Exchanger Network (HEN) Model Component")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AJP Engineering")]
-[assembly: AssemblyProduct("_HenRepositories")]
+[assembly: AssemblyProduct("_HenModel")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
