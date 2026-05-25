c829719e04079be26d2388590b5135b251943e89
Philip Giorgio <baseballstatistics@gmail.com>
2026-05-25 15:57:57 -0700

    Removed IProfilePanelData

--- Data/Profile/ProfilePanelData.cs
+++ Data/Profile/ProfilePanelData.cs
@@ -33,7 +33,9 @@
 #endregion      // HEADER
 
 #region REFERENCES
+using HenModel.Dto.Hen.Plots;
 using HenModel.Dto.Profile;
+using HenModel.Dto.Project.DefaultParameters.ProjectUnits;
 
 using System;
 using System.Collections.Generic;
@@ -46,7 +48,7 @@ using System.Threading.Tasks;
 namespace HenStudio.Data.Profile
 {
     #region public class ProfilePanelData
-    public class ProfilePanelData : IProfilePanelData
+    public class ProfilePanelData
     {
         #region CONSTANTS
         const string NAMESPACE = "HenStudio.Data.Profile";
@@ -54,11 +56,15 @@ namespace HenStudio.Data.Profile
         #endregion      // CONSTANTS
 
         #region PROPERTIES
-        public ProfileDto ProfileDtoObj { get; set; }
-        public Guid Id { get; set; }
-        public Guid ProjectId { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public Guid ProjectId { get; set; } // Project Unique Identifier
+        public Guid ProfileId { get; set; } // Profile Unique Identifier
+
+        public string HeatFlowRateUnits { get; set; }           // Heat Flow Rate PROJECT Units (e.g., kW, BTU/hr)
+        public string HeatCapacityFlowRateUnits { get; set; }   // Heat Capacity Flow Rate PROJECT Units (e.g., kW/K, BTU/hr/°F)
+        public string TemperatureUnits { get; set; }            // Temperature PROJECT Units (e.g., °C, °F)
+        public string PressureUnits { get; set; }               // Pressure PROJECT Units (e.g., bar, psi)  
+        public ProjectUnitsDto ProjectUnitsDtoObj { get; set; } // Project Units DTO Object ... EXTERN Units
+        public ProfileDto ProfileDtoObj { get; set; }           // Profile DTO Object ......... EXTERN Units
         #endregion  // PROPERTIES
 
         #region CTOR
@@ -70,82 +76,19 @@ namespace HenStudio.Data.Profile
         /// This constructor ensures that the object is in a valid default state upon creation.</remarks>
         public ProfilePanelData()
         {
-            ProfileDtoObj = new ProfileDto();
-            Id = new Guid();
-            ProjectId = new Guid();
-            Name = string.Empty; 
-            Description = string.Empty;
+            ProjectId = new Guid(); // Project Unique Identifier
+            ProfileId = new Guid(); // Profile Unique Identifier
+
+            HeatFlowRateUnits = string.Empty;           // Heat Flow Rate PROJECT Units (e.g., kW, BTU/hr)
+            HeatCapacityFlowRateUnits = string.Empty;   // Heat Capacity Flow Rate PROJECT Units (e.g., kW/K, BTU/hr/°F)
+            TemperatureUnits = string.Empty;            // Temperature PROJECT Units (e.g., °C, °F)
+            PressureUnits = string.Empty;               // Pressure PROJECT Units (e.g., bar, psi) 
+
+            ProjectUnitsDtoObj = new ProjectUnitsDto(); // ProjectUnitsDto DTO Object ... EXTERN Units
+            ProfileDtoObj = new ProfileDto();           // Profile DTO Object ........... EXTERN Units
         }
         #endregion  // CTOR
 
-        #region STRING CONVERSION METHODS
-
-        #region GetId()
-        /// <summary>
-        /// Gets the unique identifier of the project as a string.
-        /// </summary>
-        /// <returns>A string representation of the project's unique identifier.</returns>
-        public string GetId()
-        { 
-            return Id.ToString(); 
-        }
-    #endregion  // GetId()
-
-        #region GetProjectId()
-        /// <summary>
-        /// Gets the unique identifier of the project as a string.
-        /// </summary>
-        /// <returns>A string representation of the project's unique identifier.</returns>
-        public string GetProjectId()
-        {
-            return ProjectId.ToString();
-        }
-        #endregion  // GetProjectId()
-
-        #endregion  // STRING CONVERSION METHODS
-
-        #region IMPLEMENTATION of IProfilePanelData METHODS
-
-        #region ConvertToPanelData(ProfileDto profileDto)
-        /// <summary>
-        /// Creates a new ProfilePanelData instance by copying values from the specified ProfileDto object.
-        /// </summary>
-        /// <remarks>This method performs a property-by-property mapping from ProfileDto to
-        /// ProfilePanelData. All relevant fields are transferred directly. If profileDto is null, a
-        /// NullReferenceException may occur.</remarks>
-        /// <param name="profileDto">The ProfileDto object containing the source values to copy. Cannot be null.</param>
-        /// <returns>A ProfilePanelData instance populated with values from the provided ProfileDto object.</returns>
-        public ProfilePanelData ConvertToPanelData(ProfileDto profileDto)
-        {
-            ProfileDtoObj = profileDto;
-            this.Id = profileDto.Id;
-            this.ProjectId = profileDto.ProjectId;
-            this.Name = profileDto.Name;
-            this.Description = profileDto.Description;
-            return this;
-        }
-        #endregion  // ConvertToPanelData(ProfileDto profileDto)
-
-        #region ConvertFromPanelData()
-        /// <summary>
-        /// Creates a new ProfileDto instance by copying values from the current ProfilePanelData object.
-        /// </summary>
-        /// <remarks>This method performs a property-by-property mapping from ProfilePanelData to
-        /// ProfileDto. All relevant fields are transferred directly.</remarks>
-        /// NullReferenceException may occur.</remarks>
-        /// <returns>A ProfileDto instance populated with values from the provided ProfilePanelData object.</returns>
-        public ProfileDto ConvertFromPanelData()
-        {
-            ProfileDtoObj = new ProfileDto();
-            ProfileDtoObj.Id = this.Id;
-            ProfileDtoObj.ProjectId = this.ProjectId;
-            ProfileDtoObj.Name = this.Name;
-            ProfileDtoObj.Description = this.Description;
-            return ProfileDtoObj;
-        }
-        #endregion  // ConvertFromPanelData()   
-
-        #endregion  // IMPLEMENTATION of IProfilePanelData
     }
     #endregion      // public class ProfilePanelData
 }
